81fc0f6f71bd4caeca06d8184eb0bfdb4a10f7f8
Amey <abrock77@gmail.com>
2021-08-20 03:22:51 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
-function App() {
+import "./bootstrap.min.css";
+import "./App.css";
+import React, { useState ,useEffect} from "react";
+// import MovieList from "./components/Movielist";
+
+const MovieList = (props)=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  props.movies.map((movie,index)=>{
+      return (
+          <div className="rowse" >
+          <img src={movie.Poster} alt="imageof movie" height="475" width="300" ></img>
+          </div>
+      )
+  })
+  )
+}
+function App() {
+  const [movies, setMovies] = useState([])
+  const [searchValue,setsearchValue]= useState("twilight")
+  // console.log(searchValue);
+  const getmovieRequest = async (searchValue) =>{
+    console.log(searchValue);
+    const url =`http://www.omdbapi.com/?s=${searchValue}&apikey=e7b40d15`;
+    const response = await fetch(url);
+    const responeJson= await response.json();
+    if(responeJson.Search)
+    setMovies(responeJson.Search);
+   
+  }
+  useEffect(()=>{
+    getmovieRequest(searchValue);
+  },[searchValue]);
+
+  return (
+    <>
+    <div >
+    <h2 style={{display:"inline",float:"left"}}>Movies</h2>
+    <input style={{display:"inline",float:"right"} } value={searchValue} onChange={ (eve)=> setsearchValue(eve.target.value)}></input>
     </div>
+    <div className="movie-app" style={{clear:"both"}}>
+      
+        <MovieList movies={movies} />
+     
+    </div>
+ 
+    </>
   );
 }
 
